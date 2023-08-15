cd114c8ee4748aa0f2749615ee4fd4b4030b4abf
Lakshay <lakshaybansal44@gmail.com>
2023-08-15 15:22:08 +0530

    Deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+var indexValue = 1;
+showImg(indexValue);
+    function btm_slide(e){showImg(indexValue=e);}
+    function side_slide(e){showImg(indexValue+=e);}
+    function showImg(e){
+      var i;
+      const img = document.querySelectorAll('img');
+      const sliders = document.querySelectorAll('.btm-sliders span');
+      if (e>img.length){indexValue=1}
+      if (e<1){indexValue=img.length}
+      for(i=0 ; i<img.length;i++){
+        img[i].style.display = "none";
+      }
+      for(i=0 ; i<sliders.length;i++){
+        sliders[i].style.background = "rgba(255,255,255,0.1)";
+      }
+      img[indexValue-1].style.display = "block";
+      sliders[indexValue-1].style.background = "white";
+    }
